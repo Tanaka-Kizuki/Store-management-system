690aff423533f15707aa6c6adb4815244b7da04a
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-09-06 18:52:59 +0900

    ログイン認証項目の変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,13 +11,30 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        <!-- メールアドレスの入力項目を削除 
+                             <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div> -->
+                        
+                        
+                        <!-- ユーザーIDを認証項目に追加 -->
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">User ID</label>
+ 
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+ 
+                                @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
