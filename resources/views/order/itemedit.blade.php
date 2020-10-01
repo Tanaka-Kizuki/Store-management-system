b4dbf656930d50a18c436f400c3adf8c497022b3
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-02 02:46:48 +0900

    商品編集画面　フロントエンド修正

--- resources/views/order/itemedit.blade.php
+++ resources/views/order/itemedit.blade.php
@@ -1,8 +1,22 @@
-<form action="/order/item/edit" method="post">
-@csrf
-     <input type="hidden" name="id" value="{{$form->id}}">
-     <input type="text" name="name" value="{{$form->name}}">
-     <input type="number" name="price" value="{{$form->price}}">
-     <input type="number" name="base" value="{{$form->base}}">
-     <input type="submit" value="Edit">
-</form>
+@extends('layouts.app')
+
+@push('css')
+     <link rel="stylesheet" href="{{asset('/css/order.css')}}">
+@endpush
+
+@section('title','Order')
+
+@section('content')
+     <h1 class="main-title">Item Edit</h1>
+     <div class="input_form">
+          <form action="/order/item/edit" method="post">
+               @csrf
+               <input class="item_edit" type="hidden" name="id" value="{{$form->id}}">
+               <input class="item_edit" type="text" name="name" value="{{$form->name}}">
+               <input class="item_edit" type="number" name="price" value="{{$form->price}}">
+               <input class="item_edit" type="number" name="base" value="{{$form->base}}">
+               <input class="confirm" type="submit" value="Edit">
+               <button class="back"><a href="/order/item">戻る</a></button>
+          </form>
+     </div>
+@endsection
