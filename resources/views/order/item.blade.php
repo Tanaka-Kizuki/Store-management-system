f42285a729dc4696bdffe715cacc227d108a5333
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-02 02:32:48 +0900

    order 商品追加フロントエンド修正

--- resources/views/order/item.blade.php
+++ resources/views/order/item.blade.php
@@ -1,18 +1,28 @@
-<h1>Item Create</h1>
-<form action="/order/item" method="post">
-@csrf
-    <label for="name">商品名</label>
-    <input type="text" name="name" id="name">
-    <label for="price">価格</label>
-    <input type="number" name="price" id="price">
-    <label for="base">適正数値</label>
-    <input type="number" step="0.1" name="base" id="base">
+@extends('layouts.app')
 
-    <input type="submit" value="商品登録">
-</form>
+@push('css')
+     <link rel="stylesheet" href="{{asset('/css/order.css')}}">
+@endpush
 
-<div class="item">
-     <table>
+@section('title','Order')
+
+@section('content')
+<h1 class="main-title">Item Create</h1>
+<div class="input_form">
+     <form action="/order/item" method="post">
+          @csrf
+          <label for="name">商品名</label>
+          <input type="text" name="name" id="name">
+          <label for="price">価格</label>
+          <input type="number" name="price" id="price">
+          <label for="base">適正数値</label>
+          <input type="number" step="0.1" name="base" id="base">
+          <input class="confirm" type="submit" value="商品登録">
+     </form>
+</div>
+
+<div class="input_form">
+     <table class="input_table">
           <thead>
                <tr>
                     <th>商品名</th>
@@ -26,12 +36,11 @@
                     <th><p>{{$item->name}}</p></th>
                     <th>{{$item->base}}</th>
                     <th>{{$item->price}}</th>
-                    <th><a href="/order/item/edit?id={{$item->id}}">Edit</a></th>
+                    <th><a class="edit" href="/order/item/edit?id={{$item->id}}"><img src="{{asset('/image/edit.svg')}}"></a></th>
                </tr>
           @endforeach
           </tbody>
      </table>
+     <button class="back"><a href="/order">戻る</a></button>
 </div>
-
-<a href="/order">戻る</a>
-
+@endsection
