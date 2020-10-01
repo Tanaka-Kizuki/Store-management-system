d4663af6dfe6cb1761da84b9c9dbdbcac97df7d5
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-01 22:24:56 +0900

    order indexフロントエンド修正

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,10 +1,17 @@
-<style>
-a {
-     text-decoration: none;
-}
-</style>
+@extends('layouts.app')
 
-<h1>Order</h1>
-<button><a href="/order/input">発注入力</a></button>
-<button><a href="/order/history">発注履歴</a></button>
-<button><a href="/order/item">商品設定</a></button>
+@push('css')
+  <link rel="stylesheet" href="css/order.css">
+@endpush
+
+
+@section('title','Order')
+
+@section('content')
+     <h1 class="main-title">Order</h1>
+     <div class="container">
+          <a class="link button1" href="/order/input">発注入力</a>
+          <a class="link button2" href="/order/history">発注履歴</a>
+          <a class="link button3" href="/order/item">商品設定</a>
+     </div>
+@endsection
