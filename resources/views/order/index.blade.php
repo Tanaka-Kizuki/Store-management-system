bd79db0a2460e80c16e3cfc12348b2c373d075cc
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-10-03 01:20:48 +0900

    Order商品設定:管理者のみ表示

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -12,6 +12,8 @@
      <div class="container">
           <a class="link button1" href="/order/input">発注入力</a>
           <a class="link button2" href="/order/history">発注履歴</a>
+          @if(Auth::user()->admin == 0)
           <a class="link button3" href="/order/item">商品設定</a>
+          @endif
      </div>
 @endsection
