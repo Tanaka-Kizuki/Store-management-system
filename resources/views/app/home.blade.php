1f721c1115592db816bc4f1dd18b13f93c944879
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2021-02-07 22:37:28 +0900

    update home

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -3,6 +3,34 @@
 @section('title','home')
 
 @section('content')
+    <style>
+    .bgimg {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-image: url('/image/background.jpg');
+        background-size: cover;
+        z-index:0;
+    }
+    .bg {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        background-color: #ffff;
+        opacity: 0.3;
+        z-index: 0;
+    }
+    .content {
+        position: relative;
+        z-index: 2;
+        color:rgba(63, 61, 61);
+    }
+    </style>
+    <div class="bgimg"></div>
     <div class="content">
         <div class="title m-b-md">
         Cafe de Drip
@@ -14,4 +42,5 @@
             <a href="/order">Order</a>
         </div>
     </div>
+    <div class="bg"></div>
 @endsection
